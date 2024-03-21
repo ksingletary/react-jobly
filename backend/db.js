faa74a844324bf854d7bbce1b675a1eadfc03e28
ksingletary <keithsingletary@gmail.com>
2024-03-21 11:56:54 -0400

    production fixes

--- backend/db.js
+++ backend/db.js
@@ -1,23 +1,23 @@
 "use strict";
 /** Database setup for jobly. */
 const { Client } = require("pg");
+const { getDatabaseUri } = require("./config");
 
-let DB_URI;
+let db;
 
-if (process.env.NODE_ENV === "test") {
-  DB_URI = "postgresql:///jobly_test";
-} else {
-  DB_URI = "postgresql:///jobly";
-}
-const db = new Client({
-  connectionString: DB_URI
-});
-
-db.connect()
-  .then(() => console.log("Connected to the database"))
-  .catch(err => {
-    console.error("Error connecting to the database:", err.message);
-    process.exit(1); // Exit the process if unable to connect to the database
+if (process.env.NODE_ENV === "production") {
+  db = new Client({
+    connectionString: getDatabaseUri(),
+    ssl: {
+      rejectUnauthorized: false
+    }
   });
+} else {
+  db = new Client({
+    connectionString: getDatabaseUri()
+  });
+}
+
+db.connect();
 
 module.exports = db;
